675f7bbe325e3b88e5e83cfb786247a326200459
temcray <ejamison06@gmail.com>
2025-12-31 07:04:34 -0600

    update

--- portfolio/forms.py
+++ portfolio/forms.py
@@ -1,8 +1,32 @@
 from django import forms
 
 class ContactForm(forms.Form):
-  name = forms.CharField(max_length=50)
+  name = forms.CharField(
+    max_length=50,
+    widget=forms.TextInput(attrs={
+      'class': 'contact-input',
+      'placeholder' : 'Your Name'
+    })
+      
+  )
+        
+  email = forms.EmailField(
+    max_length=50,
+    widget=forms.EmailInput(attrs={
+      'class' : 'contact-input',
+      'placeholder' : 'Your Email'
+    })
+      
+  )
+    
+  message = forms.CharField(
+    widget=forms.Textarea(attrs={
+      'class' : 'contact-textarea',
+      'placeholder' : 'Your Message',
+      'rows' : 5
+    })
+    
+    )
+      
 
-  email = forms.EmailField()
   
-  message = forms.CharField(widget=forms.Textarea)
